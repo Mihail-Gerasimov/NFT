8429c4586f128ca77213e882c781a6faee432f4f
Mihail <mihailgerasimov86@gmail.com>
2022-04-22 16:01:37 +0400

    f

--- js/main.js
+++ js/main.js
@@ -27,3 +27,39 @@ let select = function () {
 };
 
 select();
+
+const slider = document.querySelector('.slider');
+const sl = new Swiper(slider, {
+    loop: true,
+    speed: 4000,
+    slidesPerView: '7.5',
+    autoplay: {
+        enabled: true,
+        delay: 0,
+    },
+    breakpoints: {
+        320: {
+            spaceBeetwen: 4,
+            slidesPerView: '3.9',
+        },
+        // when window width is >= 575px
+        575: {
+            spaceBeetwen: 8,
+            slidesPerView: '4.7',
+        },
+        // when window width is >= 767px
+        767: {
+            slidesPerView: '5.7',
+            spaceBeetwen: 12,
+        },
+        // when window width is >= 991px
+        991: {
+            slidesPerView: '6.5',
+            spaceBeetwen: 16,
+        },
+        // when window width is >= 1200px
+        1200: {
+            slidesPerView: '8.5',
+        }
+    }
+});
